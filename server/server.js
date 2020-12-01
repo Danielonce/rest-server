c12784fe5ef2eb6a8441c6162d87ef59cce1fa5f
Daniel <montalvadaniel@outlook.com>
2020-12-01 13:21:08 -0300

    RestServer user

--- server/server.js
+++ server/server.js
@@ -1,28 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 require('./config/config');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json('Hello World');
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario/', (req, res) => {
-    let body = req.body;
-    res.json({persona:body});
-})
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id  
-    res.json({
-        id
-    })
+mongoose.connect(process.env.URLDB,
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err,res)=>{
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
 })
 
 app.listen(process.env.PORT, () => {
